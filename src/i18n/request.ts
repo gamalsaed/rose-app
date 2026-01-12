ead1932560a8e540f88bfff95d196c82ae9aba8e
M-hossameldine <m.hossameldine@gmail.com>
2026-01-12 22:21:23 +0200

    feat(translation): add price and date formatting

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -12,5 +12,32 @@ export default getRequestConfig(async ({ requestLocale }) => {
   return {
     locale,
     messages: (await import(`./messages/${locale}.json`)).default,
+    formats: {
+      number: {
+        // 65,000.00
+        "price": {
+          style: "decimal",
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+          numberingSystem: locale === "ar" ? "arab" : "latn",
+        },
+      },
+      dateTime: {
+        // January 12, 2025
+        "long-date": {
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+          numberingSystem: locale === "ar" ? "arab" : "latn",
+        },
+        // Jan 12, 2025
+        "medium-date": {
+          month: "short",
+          day: "numeric",
+          year: "numeric",
+          numberingSystem: locale === "ar" ? "arab" : "latn",
+        },
+      },
+    },
   };
 });
