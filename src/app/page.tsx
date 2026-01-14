0f3cd132a23442b18b5db4719b7bd3d45637a8fc
Ahmed Hosin <ahmedhosin@Ahmeds-MacBook-Pro.local>
2026-01-14 17:20:45 +0300

    feat(home): add about, gallery, and companies sections to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import AboutSection from "@/components/features/home/about-section";
+import CompaniesSection from "@/components/features/home/companies-section";
+import GallerySection from "@/components/features/home/gallery-section";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      {/* about section */}
+      <AboutSection />
+
+      {/* allery section */}
+      <GallerySection />
+
+      {/*companies section */}
+      <CompaniesSection />
+    </main>
+  );
 }
