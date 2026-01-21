e014aaeed1b58e3959bad7d029d2ac715e5589ac
M-hossameldine <m.hossameldine@gmail.com>
2026-01-21 15:47:19 +0200

    chore(fonts): setup Edwardian Script ITC font and added it to the tailwind fonts configuration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,25 +1,33 @@
-import { hasLocale } from "next-intl";
-import { getTranslations, setRequestLocale } from "next-intl/server";
-import { notFound } from "next/navigation";
-import { routing } from "@/i18n/routing";
-import type { Metadata } from "next";
-import { Sarabun, Tajawal } from "next/font/google";
-import Providers from "@/components/providers";
+import { hasLocale } from 'next-intl';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import type { Metadata } from 'next';
+import { Sarabun, Tajawal } from 'next/font/google';
+import Providers from '@/components/providers';
+import localFont from 'next/font/local';
 
-import { Toaster } from "@/components/ui/toaster";
-// Layouts
-import Header from "@/components/layout/app/header/header";
-import Footer from "@/components/layout/app/footer/footer";
+import { Toaster } from '@/components/ui/toaster';
 
 const sarabun = Sarabun({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  variable: "--font-sarabun",
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  variable: '--font-sarabun',
 });
+
 const tajawal = Tajawal({
-  subsets: ["arabic", "latin"],
-  weight: ["200", "300", "400", "500", "700", "800", "900"],
-  variable: "--font-tajawal",
+  subsets: ['arabic', 'latin'],
+  weight: ['200', '300', '400', '500', '700', '800', '900'],
+  variable: '--font-tajawal',
+});
+
+const edwardianScript = localFont({
+  src: [
+    {
+      path: '../../../public/assets/fonts/Edwardian-Scriptitc.ttf',
+    },
+  ],
+  variable: '--font-edwardian',
 });
 
 type LocaleLayoutProps = {
@@ -29,17 +37,17 @@ type LocaleLayoutProps = {
 
 export async function generateMetadata({
   params: { locale },
-}: Pick<RouteProps, "params">): Promise<Metadata> {
+}: Pick<RouteProps, 'params'>): Promise<Metadata> {
   const t = await getTranslations({ locale });
 
   return {
-    title: t("meta-title"),
-    description: t("meta-description"),
+    title: t('meta-title'),
+    description: t('meta-description'),
   };
 }
 
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
+  return routing.locales.map(locale => ({ locale }));
 }
 
 export default async function LocaleLayout({
@@ -56,18 +64,13 @@ export default async function LocaleLayout({
   setRequestLocale(locale);
 
   return (
-    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+    <html lang={locale} dir={locale === 'ar' ? 'rtl' : 'ltr'}>
       <body
-        className={`${sarabun.variable} ${tajawal.variable} antialiased dark:bg-zinc-800 `}
+        className={`${sarabun.variable} ${tajawal.variable} ${edwardianScript.variable} antialiased dark:bg-zinc-800 `}
       >
         <Providers>
-          {/* TODO: move header and footer to a separate home page layout */}
-          <Header />
-
           {children}
           <Toaster />
-
-          <Footer />
         </Providers>
       </body>
     </html>
