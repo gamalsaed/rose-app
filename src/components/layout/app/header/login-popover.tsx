bd1790271660d21a908b934763982c685574a92d
Gamal Saed <gamalsaed557@gmail.com>
2026-02-10 17:22:00 +0200

    added translation

--- src/components/layout/app/header/login-popover.tsx
+++ src/components/layout/app/header/login-popover.tsx
@@ -16,7 +16,7 @@ import { LoginForm } from '@/app/[locale]/(auth)/login/_components/login-form';
   Added varient system to TabsTrigger component
   1-default
   2-destructive
-  3-outline
+  3-outline => pass a prop called direction, it takes 2 things left and right
   4-ghost
 */
 
@@ -44,13 +44,13 @@ export default function LoginPopover() {
               value="login"
               className="rounded-b-none rounded-r-none z-10"
             >
-              Login
+              {t('auth.login')}
             </TabsTrigger>
             <Link
               href="/register"
               className="inline-flex w-full h-full items-center justify-center rounded-none ring-2  ring-zinc-300 dark:ring-zinc-600  whitespace-nowrap dark:bg-zinc-700 rounded-tr-lg"
             >
-              Register
+              {t('auth.register')}
             </Link>
           </TabsList>
 
